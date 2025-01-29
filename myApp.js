16e3c05f710f7e91c79a763cc5b215237ba3a6ba
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2025-01-29 07:01:14 +0000

    add json path

--- myApp.js
+++ myApp.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
     res.sendFile(absolutePath);
 });
 
+app.get('/json', function (req, res) {
+    res.json({"message": "Hello json"})
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 
