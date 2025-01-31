a414014b261c49c4873a313e1fe013cf30508146
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2025-01-31 11:46:45 +0000

    .env setup

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
+require('dotenv').config(); // Load .env variables
+
 let express = require('express');
 let app = express();
-require('dotenv').config();
 
-console.log("Hello World")
+console.log("Hello World");
 
 app.get('/', function (req, res) {
     const absolutePath = __dirname + '/views/index.html';
@@ -10,47 +11,16 @@ app.get('/', function (req, res) {
 });
 
 app.get('/json', function (req, res) {
-    let message = "Hello json";
+    let message = "Hello json"
 
     if (process.env.MESSAGE_STYLE === "uppercase") {
         message = message.toUpperCase();
     }
 
-    res.json({ message: message });
+    res.json({ message });
 });
 
+// Serve static files
 app.use('/public', express.static(__dirname + '/public'));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
