4e0b75dedc5178c2e156543422304bcd11a3c1bd
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2025-01-31 11:56:02 +0000

    level up serve static

--- myApp.js
+++ myApp.js
@@ -1,22 +1,27 @@
 require('dotenv').config(); // Load .env variables
 
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 
-console.log("Hello World");
-
+// Root-Level Logger Middleware
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
-  });
-
-app.get('/', function (req, res) {
-    const absolutePath = __dirname + '/views/index.html';
-    res.sendFile(absolutePath);
 });
 
-app.get('/json', function (req, res) {
-    let message = "Hello json"
+// Serve Static Files First
+app.use('/public', express.static(__dirname + '/public'));
+
+console.log("Hello World");
+
+// Root Route
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+// JSON API Route
+app.get('/json', (req, res) => {
+    let message = "Hello json";
 
     if (process.env.MESSAGE_STYLE === "uppercase") {
         message = message.toUpperCase();
@@ -24,8 +29,5 @@ app.get('/json', function (req, res) {
 
     res.json({ message });
 });
-
-// Serve static files
-app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
